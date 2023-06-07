c59d9f4bba7ad03378329a2eb0e53e8155fb7869
gautam3189 <me.gautamvishwakarma@gmail.com>
2023-06-07 14:34:04 +0530

    DSA Solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/components/layout';
+import BubbleSortComponent from './pages/components/sort/bubble-sort.component';
+import HeapSortComponent from './pages/components/sort/heap-sort.component';
+import SelectionSortComponent from './pages/components/sort/selection-sort.component';
+import QuickSortComponent from './pages/components/sort/quick-sort.component';
+import MergeSortComponent from './pages/components/sort/merge-sort.component';
+import InsertionSortComponent from './pages/components/sort/insertion-sort.component';
+import NotFound from './pages/notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <><BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<Home />} /> */}
+          <Route path="bubbleSort" element={<BubbleSortComponent />} />
+          <Route path="heapSort" element={<HeapSortComponent />} />
+          <Route path="insertionSort" element={<InsertionSortComponent />} />
+          <Route path="mergeSort" element={<MergeSortComponent />} />
+          <Route path="quickSort" element={<QuickSortComponent />} />
+          <Route path="selectionSort" element={<SelectionSortComponent />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter >
+
+    </>
   );
 }
 
